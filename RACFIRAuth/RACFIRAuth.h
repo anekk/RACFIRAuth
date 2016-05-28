abf7d3c4e08abbcf58515efc005cfb1a2a56dded
anekk <anekk@Mac-mini-Ania.home>
2016-05-28 16:26:41 +0200

    Initial commit

---
+++ RACFIRAuth/RACFIRAuth.h
@@ -0,0 +1,19 @@
+//
+//  RACFIRAuth.h
+//  RACFIRAuth
+//
+//  Created by Ania on 28.05.2016.
+//  Copyright © 2016 ankowska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RACFIRAuth.
+FOUNDATION_EXPORT double RACFIRAuthVersionNumber;
+
+//! Project version string for RACFIRAuth.
+FOUNDATION_EXPORT const unsigned char RACFIRAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RACFIRAuth/PublicHeader.h>
+
+
---
+++ RACFIRAuth/RACFIRAuth.swift
@@ -0,0 +1,11 @@
+//
+//  RACFIRAuth.swift
+//  RACFIRAuth
+//
+//  Created by Ania on 28.05.2016.
+//  Copyright © 2016 ankowska. All rights reserved.
+//
+
+import Foundation
+
+
